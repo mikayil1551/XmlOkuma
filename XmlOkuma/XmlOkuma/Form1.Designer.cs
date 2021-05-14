c7992a9d09eed1228bbe0de4b7acb50de41993d6
Sadigzade Mikayil <75858136+mikayil1551@users.noreply.github.com>
2021-05-15 02:14:08 +0300

    Add files via upload

---
+++ XmlOkuma/XmlOkuma/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace XmlOkuma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAciklama = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listAciklamalar = new System.Windows.Forms.ListBox();
+            this.listBasliklar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblAciklama
+            // 
+            this.lblAciklama.AutoSize = true;
+            this.lblAciklama.Location = new System.Drawing.Point(291, -17);
+            this.lblAciklama.Name = "lblAciklama";
+            this.lblAciklama.Size = new System.Drawing.Size(0, 13);
+            this.lblAciklama.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(378, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Haber Detayi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Haber Basliklari";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(354, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = ">";
+            // 
+            // listAciklamalar
+            // 
+            this.listAciklamalar.HorizontalScrollbar = true;
+            this.listAciklamalar.Location = new System.Drawing.Point(382, 48);
+            this.listAciklamalar.Name = "listAciklamalar";
+            this.listAciklamalar.Size = new System.Drawing.Size(394, 420);
+            this.listAciklamalar.TabIndex = 5;
+            // 
+            // listBasliklar
+            // 
+            this.listBasliklar.FormattingEnabled = true;
+            this.listBasliklar.Location = new System.Drawing.Point(29, 48);
+            this.listBasliklar.Name = "listBasliklar";
+            this.listBasliklar.Size = new System.Drawing.Size(319, 420);
+            this.listBasliklar.TabIndex = 6;
+            this.listBasliklar.SelectedIndexChanged += new System.EventHandler(this.listBasliklar_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAciklama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listAciklamalar);
+            this.Controls.Add(this.listBasliklar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAciklama;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listAciklamalar;
+        private System.Windows.Forms.ListBox listBasliklar;
+    }
+}
+
---
+++ XmlOkuma/XmlOkuma/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XmlOkuma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        XmlDocument doc;
+        XmlNode haberler;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            doc = new XmlDocument();
+            doc.Load("..\\..\\Haberler.xml");
+            haberler = doc.SelectSingleNode("haberler");
+            //this.Text = haberler.SelectSingleNode("baslik").InnerText;
+            lblAciklama.Text = string.Format(@"{0} - {1}", haberler.SelectSingleNode("aciklama").InnerText,
+                haberler.SelectSingleNode("tarih").InnerText);
+
+            XmlNodeList haberList = haberler.SelectNodes("haber");
+            foreach (XmlNode haber in haberList)
+            {
+                listBasliklar.Items.Add(haber.SelectSingleNode("baslik").InnerText);                
+            }
+        }
+
+        private void listBasliklar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string baslik = listBasliklar.SelectedItem.ToString();
+            XmlNodeList haberList = haberler.SelectNodes("haber");
+            foreach (XmlNode hbr in haberList)
+            {
+                string bsl = hbr.SelectSingleNode("baslik").InnerText;
+                if (bsl==baslik)
+                {
+                    listAciklamalar.Items.Add(hbr.SelectSingleNode("aciklama").InnerText);
+                }
+
+            }
+
+        }
+    }
+}
